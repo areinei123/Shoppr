2c9c437282b558e25fe17bea11eb268f42a38ee2
Fred Chapman <areinei123@gmail.com>
2015-06-06 17:05:05 -0400

    fixing merge conflicts, tests not passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606194615) do
+ActiveRecord::Schema.define(version: 20150606203331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "grocery_lists", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "grocery_lists", ["group_id"], name: "index_grocery_lists_on_group_id", using: :btree
 
   create_table "groups", force: :cascade do |t|
     t.string "name", null: false
@@ -45,4 +54,5 @@ ActiveRecord::Schema.define(version: 20150606194615) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "grocery_lists", "groups"
 end
