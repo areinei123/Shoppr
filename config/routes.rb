1ba31ac7c247db5ec0d1275f64c9587c7d9e9674
genagain <ohta.g@husky.neu.edu>
2015-06-06 15:23:44 -0400

    fixing the tests after adding the the first name and last name to the users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'groups#index'
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations' }
 
   resources :groups, only: [:index]
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -3,6 +3,8 @@ require 'factory_girl'
 FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    first_name 'Joe'
+    last_name 'Shmoe'
     password 'password'
     password_confirmation 'password'
   end
