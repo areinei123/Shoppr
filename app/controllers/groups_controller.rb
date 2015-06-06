7e3798e19fcdcda981e04e0138f6cd6bb5a49fa8
Dodie324 <drmoore324@gmail.com>
2015-06-06 16:11:16 -0400

    Add create action in groups controller and build form to create new group. Write tests to validate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,21 @@
 class GroupsController < ApplicationController
   def index
     @groups = Group.all
+    @group  = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      flash[:notice] = "Group added."
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:name)
   end
 end
