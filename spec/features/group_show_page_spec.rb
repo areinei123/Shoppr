b3fc439e2081884d3c0875038e31edc054bac798
Fred Chapman <areinei123@gmail.com>
2015-06-06 15:22:51 -0400

    minor test fixes

--- spec/features/group_show_page_spec.rb
+++ spec/features/group_show_page_spec.rb
@@ -1,20 +1,13 @@
-require 'rails-helper'
+require 'rails_helper'
 
 feature 'user visits show page' do
+  let!(:user) {FactoryGirl.create(:user)}
+  let!(:group) {FactoryGirl.create(:group)}
   context 'As an authenticated user' do
-    before (:each) do
-      user = FactoryGirl.create(:user)
-      group = FactoryGirl.create(:group)
-      visit new_user_session_path
-
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-
-      click_button 'Log in'
-    end
     scenario 'I want to be able to view the show page' do
       visit root_path
-      click_on group.name
+      sign_in_as(user)
+      click_on "#{group.name}"
       expect(page).to have_content("#{group.name}")
     end
   end
