4a0f5901537294d5768538e1613ddaa148e95b77
David Rodriguez Fuentes <davidrf09@gmail.com>
2015-06-07 12:45:21 -0400

    fix things

---
+++ spec/features/receipts/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'Create a new recipe' do
+  let(:group) { FactoryGirl.create(:group_with_grocery_list) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:membership) { FactoryGirl.create(:membership, user: user, group: group) }
+  let(:receipt) { FactoryGirl.create(:receipt,
+                    membership: membership,
+                    grocery_list: group.grocery_list
+                )}
+
+  scenario 'see all receipts' do
+    visit root_path
+    sign_in_as user
+    click_link group.name
+  end
+end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -11,9 +11,25 @@ FactoryGirl.define do
 
   factory :group do
     sequence(:name) { |n| "group #{n}" }
+    factory :group_with_grocery_list do
+      after(:create) do |group|
+        FactoryGirl.create(:grocery_list, group: group)
+      end
+    end
+  end
+
+  factory :membership do
+    user
+    group
   end
 
   factory :grocery_list do
     sequence(:name) { |n| "New List #{n}"}
+  end
+
+  factory :receipt do
+    photo "#{Rails.root}/spec/support/images/receipt.jpg"
+    membership
+    grocery_list
   end
 end
